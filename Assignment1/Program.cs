daba8b10fc292ba1b021661f3272cf33dbd68ff0
rafifazzaki <59862154+rafifazzaki@users.noreply.github.com>
2024-02-26 11:04:38 +0700

    first commit

---
+++ Assignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿//3 foo
+// 5 bar
+// User input => n
+// n = 15
+// 0, 1, 2, foo, 4, bar, foo, 7, 8, foo, bar, 11, foo, 13, 14, foobar
+
+using InternalCalculationLib;
+class Program{
+    static void Main(){
+        string input = Console.ReadLine();
+        string[] a = ChangeToFooAndBar.Run(input);
+
+        Console.WriteLine(ChangeToFooAndBar.Run(input));
+        foreach(string number in a){
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ InternalCalculationLib/ChangeToFooAndBar.cs
@@ -0,0 +1,69 @@
+﻿namespace InternalCalculationLib;
+
+public class ChangeToFooAndBar{
+    public static void RunConsole(string input){
+        int.TryParse(input, out int a);
+        Console.WriteLine("0");
+        for(int i=1; i<a+1; i++){
+            if(i%3 == 0 && i%5 == 0){
+                Console.WriteLine("Foobar");
+            }else if(i%3 == 0){
+                Console.WriteLine("Foo");
+            }else if(i%5 == 0){
+                Console.WriteLine("bar");
+            }else{
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    public static string[] Run(string input){
+        int.TryParse(input, out int inputInt);
+        
+        string[] arrayNumber = new string[inputInt];
+        // inputInt = 15
+        // tapi arrayNumber[0-14]
+        // int i = 0;
+        // do{
+        //     if(i%3 == 0 && i%5 == 0){
+        //         arrayNumber[i] = "Foobar";
+        //     }else if(i%3 == 0){
+        //         arrayNumber[i] = "Foo";
+        //     }else if(i%5 == 0){
+        //         arrayNumber[i] = "bar";
+        //     }else{
+        //         arrayNumber[i] = i.ToString();
+        //     }
+        //     i++;
+        // }while(i<inputInt);
+
+        // while(i<inputInt){
+        //     if(i%3 == 0 && i%5 == 0){
+        //         arrayNumber[i] = "Foobar";
+        //     }else if(i%3 == 0){
+        //         arrayNumber[i] = "Foo";
+        //     }else if(i%5 == 0){
+        //         arrayNumber[i] = "bar";
+        //     }else{
+        //         arrayNumber[i] = i.ToString();
+        //     }
+        //     i++;
+        // }
+        
+        
+        for(int i=0; i<inputInt; i++){
+            
+            if(i%3 == 0 && i%5 == 0){
+                arrayNumber[i] = "Foobar";
+            }else if(i%3 == 0){
+                arrayNumber[i] = "Foo";
+            }else if(i%5 == 0){
+                arrayNumber[i] = "bar";
+            }else{
+                arrayNumber[i] = i.ToString();
+            }
+        }
+        arrayNumber[0] = "0";
+        return arrayNumber;
+    }
+}
