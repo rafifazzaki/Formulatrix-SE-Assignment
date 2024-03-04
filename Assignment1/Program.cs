961b1325f5d6f93a4b7860c958ba15104e732e7b
Rafif Azzaki <rafifazzaki@ymail.com>
2024-03-04 16:26:51 +0700

    user input

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -6,19 +6,58 @@
 
 using System.Text;
 using InternalCalculationLib;
-class Program{
-    static void Main(){
-        string input = Console.ReadLine();
-
+class Program
+{
+    static void Main()
+    {
         ChangeToFooAndBar fb = new();
-        fb.AddRules(10, "baz");
-        
 
-        string[] a = fb.Run(input); //return string[]
-        foreach(string number in a){
-            Console.WriteLine(number);
-        }
+        string? answer;
+        string? addRuleInt;
+        string? addRuleString;
 
-        
+        string? input;
+        bool isEnding = false;
+
+        do
+        {
+            Console.WriteLine("Add rule?");
+            Console.WriteLine("1. Yes");
+            Console.WriteLine("2. No, Go to input");
+            answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "1":
+                    Console.WriteLine("=======================");
+                    Console.WriteLine("Number to change:");
+                    addRuleInt = Console.ReadLine();
+                    Console.WriteLine("to string:");
+                    addRuleString = Console.ReadLine();
+                    int.TryParse(addRuleInt, out int output);
+                    fb.AddRules(output, addRuleString);
+
+                    break;
+                case "2":
+                    Console.WriteLine("=======================");
+                    Console.WriteLine("Input number:");
+                    input = Console.ReadLine();
+                    string[] a = fb.Run(input); //return string[]
+                    foreach (string number in a)
+                    {
+                        Console.WriteLine(number);
+                    }
+                    isEnding = true;
+                    break;
+                default:
+
+                    break;
+            }
+
+        } while (!isEnding);
+
+
+
+
     }
 }
--- Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bf1ae7e8998aeb267ee3c3ee1911837a53bf4a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52729d73a1e4cb8cc84b21980eb8ac69154a29f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
